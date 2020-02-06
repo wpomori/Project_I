078ca304ea07fc8d1e916984eee7a1a5586749c0
Wellington Omori <wpomori@gmail.com>
2020-02-06 18:42:43 -0300

    Create the function from showed screen of game

---
+++ alien_invasion.py
@@ -0,0 +1,22 @@
+import sys
+import pygame
+
+
+def run_game():
+    # Inicializa o jogo e cria um objeto para a tela
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Inicializa o laço principal do jogo
+    while True:
+        # Observa eventos de teclado e de mouse
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        # Deixa a tela mais recente visível
+        pygame.display.flip()
+
+
+run_game()

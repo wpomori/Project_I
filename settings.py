1d7db3fa539787d3d79851af7b03e4b971577f26
Wellington Omori <wpomori@gmail.com>
2020-02-06 18:59:23 -0300

    Create settings file and defing Settings class

---
+++ settings.py
@@ -0,0 +1,15 @@
+class Settings():
+    """
+    Uma classe para armazenar todas as configurações da Invasão Alienígena.
+    """
+
+
+    def __init__(self):
+        """
+        Inicializa as configurações do jogo.
+        """
+
+        # Configurações da tela
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)

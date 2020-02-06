f3422c07c2b6fcd5a6d8d371c6aa81b28bb26d3d
Wellington Omori <wpomori@gmail.com>
2020-02-06 18:48:18 -0300

    Fix screen background color to light gray

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,12 +8,18 @@ def run_game():
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
 
+    # Define cor de fundo
+    bg_color = (230, 230, 230)
+
     # Inicializa o laço principal do jogo
     while True:
         # Observa eventos de teclado e de mouse
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        
+        # Redesenha a tela a cada passagem pelo laço
+        screen.fill(bg_color)
         
         # Deixa a tela mais recente visível
         pygame.display.flip()

7e20ac2187588c50b4a3eab1bba7ef87ba568c4d
Wellington Omori <wpomori@gmail.com>
2020-05-13 00:53:26 -0300

    Instance assignment of the Settings Class that configures the screen display

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,18 @@
 import sys
 import pygame
 
+from settings import Settings
+
 
 def run_game():
     # Inicializa o jogo e cria um objeto para a tela
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption("Alien Invasion")
 
-    # Define cor de fundo
-    bg_color = (230, 230, 230)
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height)
+    )
+    pygame.display.set_caption("Alien Invasion")
 
     # Inicializa o laço principal do jogo
     while True:
@@ -19,7 +22,7 @@ def run_game():
                 sys.exit()
         
         # Redesenha a tela a cada passagem pelo laço
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
         
         # Deixa a tela mais recente visível
         pygame.display.flip()
